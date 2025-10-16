9bdfeb505fa878c39d4a1d26ea92f2e798bfc7ff
dannitacode <dannavalenchu076@gmail.com>
2025-10-16 20:12:08 -0300

    Estructuro la conexión a la base de datos

--- src/Persistencia/Conexion.java
+++ src/Persistencia/Conexion.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Persistencia;
-
+import java.sql.*;
 /**
  *
  * @author matute
  */
 public class Conexion {
+    static String url = "jdbc:mariadb://localhost:3306/sparelax";
+    static String user = "root";
+    static String password = "";
+    
+    public static Connection establecerConexion() {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexion exitosa.");
+        } catch (SQLException s) {
+            s.getMessage();
+        }
+        return con;
+    }
     
 }
