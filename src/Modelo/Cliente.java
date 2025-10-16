b98c04739310e25faed35e7b7a50cc7fe01ceb0b
MatiasHauria <hauriatute@gmail.com>
2025-10-16 19:59:24 -0300

    "Se colocan los atributos y datos a la entidad Cliente"

--- src/Modelo/Cliente.java
+++ src/Modelo/Cliente.java
@@ -9,5 +9,81 @@ package Modelo;
  * @author matute
  */
 public class Cliente {
+    private int codCli = -1;
+    private int dni;
+    private String nombreCompleto;
+    private int telefono;
+    private int edad;
+    private String afecciones;
+    private boolean estado = false;
+
+    public Cliente(int dni, String nombreCompleto, int telefono, int edad, String afecciones) {
+        this.dni = dni;
+        this.nombreCompleto = nombreCompleto;
+        this.telefono = telefono;
+        this.edad = edad;
+        this.afecciones = afecciones;
+    }
+
+    public int getCodCli() {
+        return codCli;
+    }
+
+    public void setCodCli(int codCli) {
+        this.codCli = codCli;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getAfecciones() {
+        return afecciones;
+    }
+
+    public void setAfecciones(String afecciones) {
+        this.afecciones = afecciones;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return  nombreCompleto;
+    }
     
 }
