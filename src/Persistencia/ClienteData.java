61129c930fa8c4a07434d0b0722de19d0360969d
MatiasHauria <hauriatute@gmail.com>
2025-10-16 20:10:52 -0300

    "Update ClienteData"

--- src/Persistencia/ClienteData.java
+++ src/Persistencia/ClienteData.java
@@ -4,10 +4,51 @@
  */
 package Persistencia;
 
+import Modelo.Cliente;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.mariadb.jdbc.Connection;
+
 /**
  *
  * @author matute
  */
 public class ClienteData {
+    private Connection con = null;
+
+    public ClienteData(Conexion a) {
+        this con = a.BuscarConexion();
+    }
+    public void insertarAlumno(Cliente e) {
+        String query = "INSERT INTO cliente(dni,nombre_completo,telefono,edad,afecciones,estado) VALUES(?,?,?,?,?,?) ";
+        try {
+            PreparedStatement ps = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, e.getDni());
+            ps.setString(2, e.getNombreCompleto());
+            ps.setInt(3, e.getTelefono());
+            ps.setInt(4, e.getEdad());
+            ps.setString(5, e.getAfecciones());
+            ps.setBoolean(6, e.isEstado());
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                e.setCodCli(rs.getInt(1));
+            } else {
+                System.out.println("No se pudo tener el ID ");
+            }
+
+            ps.close();
+            System.out.println("Alumno guardado correctamente");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    
+    
     
 }
