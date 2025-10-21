37190e654393803932a687b64d1ad3ca54f9948d
josemir01 <josemirzaleh45@gmail.com>
2025-10-21 18:55:44 -0300

    Masajista

---
+++ src/Modelo/Masajista.java
@@ -0,0 +1,76 @@
+
+package Modelo;
+
+
+public class Masajista {
+  private String matricula;
+  private String  nombre;
+  private String  apellido;
+  private int telefono;
+  private String  especialidad;
+  private boolean estado;
+
+    public Masajista(String matricula, String nombre, String apellido, int telefono, String especialidad, boolean estado) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.especialidad = especialidad;
+        this.estado = estado;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return  "nombre=" + nombre + ", apellido=" + apellido + ", especialidad=" + especialidad ;
+    }
+  
+  
+}
