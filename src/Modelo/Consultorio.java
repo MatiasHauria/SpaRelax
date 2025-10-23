e56b87810fb2c08d32666b5ba18f39631e494538
Agus-d-e <elpetiagus@gmail.com>
2025-10-23 19:13:52 -0300

    Creacion ConsultorioData

--- src/Modelo/Consultorio.java
+++ src/Modelo/Consultorio.java
@@ -1,14 +1,22 @@
 package Modelo;
 
 public class Consultorio {
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
     private int idConsultorio;
     private String usos;
     private String equipamiento;
     private boolean apto;
 
+<<<<<<< Updated upstream
     public Consultorio(String usos, String equipamiento, boolean apto) {
         this.idConsultorio = -1;
+=======
+    public Consultorio(int idConsultorio, String equipamiento, String usos, boolean apto) {
+        this.idConsultorio = idConsultorio;
+>>>>>>> Stashed changes
         this.usos = usos;
         this.equipamiento = equipamiento;
         this.apto = apto; // SUjeto a cambios. Debería inicializarse en false (?
@@ -30,8 +38,13 @@ public class Consultorio {
         return apto;
     }
 
+<<<<<<< Updated upstream
     public void setIdConsultorio(int idConsultorio) {
         this.idConsultorio = idConsultorio;
+=======
+    public void setNroConsultorio(int nroConsultorio) {
+        this.idConsultorio = nroConsultorio;
+>>>>>>> Stashed changes
     }
 
     public void setUsos(String usos) {
@@ -48,7 +61,11 @@ public class Consultorio {
 
     @Override
     public String toString() {
+<<<<<<< Updated upstream
         return "Nro de consultorio: " + this.idConsultorio + " | " + "Usos: " + this.usos + " | " + "Equipamiento: " + this.equipamiento + " | " + "Apto: " + this.apto;
+=======
+        return "Consultorio{" + "idConsultorio=" + idConsultorio + ", usos=" + usos + ", equipamiento=" + equipamiento + ", apto=" + apto + '}';
+>>>>>>> Stashed changes
     }
 
 }
