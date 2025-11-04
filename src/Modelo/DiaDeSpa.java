6db89f2bab7aaa89cc03608bf159dc2eb3184145
MatiasHauria <hauriatute@gmail.com>
2025-11-04 19:03:36 -0300

    "se agregan los modelos de las troncales"

--- src/Modelo/DiaDeSpa.java
+++ src/Modelo/DiaDeSpa.java
@@ -4,10 +4,95 @@
  */
 package Modelo;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 /**
  *
  * @author Juan
  */
 public class DiaDeSpa {
+    private int codPack = -1;
+    private Cliente cliente;
+    private int idCliente;
+    private ArrayList<Sesion> sesiones;
+    private LocalDate fechayHora;
+    private String preferencias;
+    private double monto;
+    private boolean estado;
+
+    public DiaDeSpa(Cliente cliente, ArrayList<Sesion> sesiones, LocalDate fechayHora, String preferencias, double monto, boolean estado) {
+        this.cliente = cliente;
+        this.idCliente = cliente.getIdCliente();
+        this.sesiones = sesiones;
+        this.fechayHora = fechayHora;
+        this.preferencias = preferencias;
+        this.monto = monto;
+        this.estado = estado;
+    }
+
+    public int getCodPack() {
+        return codPack;
+    }
+
+    public void setCodPack(int codPack) {
+        this.codPack = codPack;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public ArrayList<Sesion> getSesiones() {
+        return sesiones;
+    }
+
+    public void setSesiones(ArrayList<Sesion> sesiones) {
+        this.sesiones = sesiones;
+    }
+
+    public LocalDate getFechayHora() {
+        return fechayHora;
+    }
+
+    public void setFechayHora(LocalDate fechayHora) {
+        this.fechayHora = fechayHora;
+    }
+
+    public String getPreferencias() {
+        return preferencias;
+    }
+
+    public void setPreferencias(String preferencias) {
+        this.preferencias = preferencias;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
     
 }
--- src/Modelo/Sesion.java
+++ src/Modelo/Sesion.java
@@ -4,10 +4,145 @@
  */
 package Modelo;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 /**
  *
  * @author Juan
  */
 public class Sesion {
-    
+    private int codSesion = -1;
+    private Consultorio consultorio;
+    private int codConsultorio;
+    private Tratamiento tratamiento;
+    private int codTratamiento;
+    private DiaDeSpa diadespa;
+    private int codPack;
+    private ArrayList<Instalacion> instalacion;
+    private Masajista masajista;
+    private int matricula;
+    private LocalDate fechaHoraInicio;
+    private LocalDate fechaHoraFin;
+    private boolean estado;
+
+    public Sesion(Consultorio consultorio, Tratamiento tratamiento, DiaDeSpa diadespa, ArrayList<Instalacion> instalacion, Masajista masajista, LocalDate fechaHoraInicio, LocalDate fechaHoraFin, boolean estado) {
+        this.consultorio = consultorio;
+        this.codConsultorio = consultorio.getNroConsultorio();
+        this.tratamiento = tratamiento;
+        this.codTratamiento = tratamiento.getIdTratamiento();
+        this.diadespa = diadespa;
+        this.codPack = diadespa.getCodPack();
+        this.instalacion = instalacion;
+        this.masajista = masajista;
+        this.matricula = masajista.getMatricula();
+        this.fechaHoraInicio = fechaHoraInicio;
+        this.fechaHoraFin = fechaHoraFin;
+        this.estado = estado;
+    }
+
+    public int getCodSesion() {
+        return codSesion;
+    }
+
+    public void setCodSesion(int codSesion) {
+        this.codSesion = codSesion;
+    }
+
+    public Consultorio getConsultorio() {
+        return consultorio;
+    }
+
+    public void setConsultorio(Consultorio consultorio) {
+        this.consultorio = consultorio;
+    }
+
+    public int getCodConsultorio() {
+        return codConsultorio;
+    }
+
+    public void setCodConsultorio(int codConsultorio) {
+        this.codConsultorio = codConsultorio;
+    }
+
+    public Tratamiento getTratamiento() {
+        return tratamiento;
+    }
+
+    public void setTratamiento(Tratamiento tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+
+    public int getCodTratamiento() {
+        return codTratamiento;
+    }
+
+    public void setCodTratamiento(int codTratamiento) {
+        this.codTratamiento = codTratamiento;
+    }
+
+    public DiaDeSpa getDiadespa() {
+        return diadespa;
+    }
+
+    public void setDiadespa(DiaDeSpa diadespa) {
+        this.diadespa = diadespa;
+    }
+
+    public int getCodPack() {
+        return codPack;
+    }
+
+    public void setCodPack(int codPack) {
+        this.codPack = codPack;
+    }
+
+    public ArrayList<Instalacion> getInstalacion() {
+        return instalacion;
+    }
+
+    public void setInstalacion(ArrayList<Instalacion> instalacion) {
+        this.instalacion = instalacion;
+    }
+
+    public Masajista getMasajista() {
+        return masajista;
+    }
+
+    public void setMasajista(Masajista masajista) {
+        this.masajista = masajista;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public LocalDate getFechaHoraInicio() {
+        return fechaHoraInicio;
+    }
+
+    public void setFechaHoraInicio(LocalDate fechaHoraInicio) {
+        this.fechaHoraInicio = fechaHoraInicio;
+    }
+
+    public LocalDate getFechaHoraFin() {
+        return fechaHoraFin;
+    }
+
+    public void setFechaHoraFin(LocalDate fechaHoraFin) {
+        this.fechaHoraFin = fechaHoraFin;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
 }
